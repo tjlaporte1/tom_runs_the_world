85dd2724bc0f4720f0822ef7f3eef11b5ecc9e4c
tjlaporte1 <tjlaporte1@gmail.com>
2025-03-23 22:40:50 -0400

    minor edits

--- pages/gear.py
+++ pages/gear.py
@@ -1,24 +1,20 @@
 import pandas as pd
 import streamlit as st
 
-from main import df, max_date
+import main as m
 
 # distinct gear type list
-gear_list = df['name_gear'].value_counts().index.tolist()
-gear_list = gear_list[~pd.isnull(gear_list)]
+gear_list = m.df['name_gear'].value_counts().index.tolist()
 gear_list.insert(0, 'All')
 
-page_head = st.container()
-page_head.title('Tom Runs The World')
-page_head.subheader('Strava Data Analysis')
-page_head.caption('Data as of: ' + max_date)
-page_head.button('Refresh Data')
-page_head.divider()
+with st.container():
+    st.title('Tom Runs The World')
+    st.subheader('Strava Data Analysis')
+    st.caption('Data as of: ' + m.max_date)
+    st.divider()
 
-filter_col1, filter_col2 = st.columns([7,1])
+filter_col1, filter_col2 = st.columns([2, 1])
 with filter_col1:
-    st.subheader('Activity Type')
-    act_type_selection = st.segmented_control('Activity Type', ['All', 'distinct activity types'])
+    act_type_selection = st.segmented_control('Activity Type', m.act_type_filter, default='All')
 with filter_col2:
-    st.subheader('Year')
-    year_selection = st.selectbox('Years', [2025, 2024, 'All', 'Rolling 12 mo'])
+    year_selection = st.selectbox('Years', m.year_filter, index=1)
